962b77ce999fd99a8b0b476b41bf06bba4b57413
programandoconro <programandoconro@gmail.com>
2023-05-11 21:59:58 +0900

    add clean function

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,47 @@
-use std::io;
 use aho_corasick::AhoCorasick;
+use std::io;
 
-
-fn main() -> io::Result<()>{
+fn main() -> io::Result<()> {
     println!("Init");
     let input = concatenate_input();
     let clean_input = clean(input);
     println!("{}", clean_input);
 
-        Ok(())
+    Ok(())
 }
 
 fn concatenate_input() -> String {
-
     let mut input = String::new();
     let reader = io::stdin().lines();
-    for (i, line) in  reader.enumerate() {
+    for (i, line) in reader.enumerate() {
         let new_line = str::replace(&line.unwrap(), " ", "-");
         if i == 0 {
-           input = new_line.clone();
+            input = new_line.clone();
+        } else {
+            input = input + &"-".to_string() + &new_line;
         }
-        else{
-           input = input + &"-".to_string() +  &new_line;
-        }
-
     }
-        input
+    input
 }
 
 fn clean(haystack: String) -> String {
-    let patterns = &["@", "/"];
-    let replace_with = &["", "-"];
-
+    let patterns = &[
+        "@", "/", " ", "^", "~", ":", "*", "?", "[", "]", "#", "$", "%", "&", "+", "=",
+        "(", ")", "!", "'", "\"", "#", "+"
+    ];
+    let replace_with = &["-"].repeat(patterns.len());
     let ac = AhoCorasick::new(patterns);
     let result = ac.unwrap().replace_all(&haystack, replace_with);
+    let mut clean: String = String::new();
 
-    result
-    
+    for (i, current_char) in result.chars().enumerate() {
+        let next_char = result.chars().nth(i + 1).unwrap_or('-');
+        if current_char == '-' && next_char == '-' ||
+           clean.len() == 0 && current_char == '-' {
+            continue;
+        }
+            clean.push(current_char);
+    }
 
+    clean
 }
