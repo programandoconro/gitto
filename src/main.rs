6a157c384a94164ea26c30e8404788cc8220135a
programandoconro <programandoconro@gmail.com>
2023-05-14 00:46:20 +0900

    add ctrl-c handler

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,55 @@
 use aho_corasick::AhoCorasick;
-use std::io;
+use std::io::{self, BufRead};
 use std::process::Command;
 
-fn main() -> io::Result<()> {
+fn main() {
     println!("Init");
-    let input = read_user_input();
-    let concatenated_input = clean(concatenate_input(input));
-    let command = git_checkout(concatenated_input.clone());
-    println!("{}", command);
-    let mut input_to_confirm = String::new();
-    io::stdin().read_line(&mut input_to_confirm).unwrap();
 
-    let modified_command = command.trim();
+    let input = read_user_input();
+    let concatenated_input = sanitize(concatenate(input));
+    let command = git_checkout(concatenated_input);
+    println!("{}", command);
+
+    if confirm() {
+        execute(command).expect("There was an error");
+    }
+}
+
+fn execute(command: String) -> Result<i32, ()> {
     let output = Command::new("sh")
         .arg("-c")
-        .arg(modified_command)
+        .arg(command)
         .output()
         .expect("failed to execute command");
 
     if output.status.success() {
         let stdout = String::from_utf8_lossy(&output.stdout);
         println!("{}", stdout);
+        Ok(0)
     } else {
         let stderr = String::from_utf8_lossy(&output.stderr);
         eprint!("{}", stderr);
+        Err(())
     }
-    Ok(())
+}
+
+fn confirm() -> bool {
+    let stdin = io::stdin();
+    let mut input = String::new();
+
+    ctrlc::set_handler(move || {
+        println!("Bye...");
+        std::process::exit(1); // exit the program
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    match stdin.lock().read_line(&mut input) {
+        Ok(_) => true,
+        Err(e) => {
+            eprintln!("Error reading input: {}", e);
+            false
+        }
+    }
 }
 
 fn git_checkout(branch_name: String) -> String {
@@ -39,7 +63,7 @@ fn read_user_input() -> Vec<String> {
         .collect()
 }
 
-fn concatenate_input(input: Vec<String>) -> String {
+fn concatenate(input: Vec<String>) -> String {
     let mut concatenated: String = "".to_string();
     for line in input {
         if concatenated.len() == 0 {
@@ -52,7 +76,7 @@ fn concatenate_input(input: Vec<String>) -> String {
     concatenated
 }
 
-fn clean(haystack: String) -> String {
+fn sanitize(haystack: String) -> String {
     let patterns = &[
         "@", "/", " ", "^", "~", ":", "*", "?", "[", "]", "#", "$", "%", "&", "+", "=", "(", ")",
         "!", "'", "\"", "#", "+", ">", "<", "?",
@@ -76,18 +100,25 @@ fn clean(haystack: String) -> String {
 #[test]
 fn it_handles_line_breaks() {
     let input = vec!["hola".to_string(), "chao".to_string()];
-    let concatenated = concatenate_input(input);
+    let concatenated = concatenate(input);
     assert_eq!(concatenated, "hola-chao".to_string());
 }
 
 #[test]
-fn it_cleans_forbidden_chars() {
+fn it_sanitazes_forbidden_chars() {
     let input = "hola!@#$%^&*()+=?><chao";
-    let cleaned = clean(input.to_string());
+    let cleaned = sanitize(input.to_string());
     assert_eq!(cleaned, "hola-chao".to_string());
 }
+
 #[test]
 fn it_returns_checkout_command() {
     let command = git_checkout("my-new-branch".to_string());
     assert_eq!(command, "git checkout -b my-new-branch".to_string());
+}
+
+#[test]
+fn it_executes_shell_commands() {
+    let result = execute(format!("echo hola"));
+    assert_eq!(result.unwrap(), 0);
 }
