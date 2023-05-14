89d5dc255c184c4d12d690b7e246467398c7927c
programandoconro <programandoconro@gmail.com>
2023-05-14 20:03:56 +0900

    parse arguments in program

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 mod commands;
+use clap::{ValueEnum, Parser};
+
+
+#[derive(ValueEnum, Clone, Debug)]
+enum Command {
+    Switch,
+    Create
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    command: Command,
+}
 
 fn main() {
     println!("Init");
+    let args = Args::parse();
 
-    commands::branch::switch();
-//    commands::branch::create();
+    
+    match args.command {
+        Command::Create => commands::branch::create(),
+        Command::Switch => commands::branch::switch(),
+    } 
 
     println!("Finished");
 
