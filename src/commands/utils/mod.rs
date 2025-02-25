f4ce0b926a01115c3b165454ff730f4974f98c6d
Ro <50117686+programandoconro@users.noreply.github.com>
2025-02-25 17:19:27 +0900

    Update mod.rs

--- src/commands/utils/mod.rs
+++ src/commands/utils/mod.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use std::io::{self, BufRead, Error};
 use std::process::Command;
 
@@ -5,7 +6,7 @@ pub fn command_output(command: &str, args: Vec<&str>) -> String {
     let output = Command::new(command)
         .args(&args)
         .output()
-        .expect("Failed to execute command");
+        .expect(&"Failed to execute command".red());
 
     let stdout = String::from_utf8(output.stdout).expect("Invalid UTF-8");
 
@@ -32,7 +33,7 @@ pub fn execute(command: &str) -> Result<i32, i32> {
         .arg("-c")
         .arg(command)
         .output()
-        .expect("failed to execute command");
+        .expect(&"failed to execute command".red());
 
     if output.status.success() {
         let stdout = String::from_utf8_lossy(&output.stdout);
@@ -48,12 +49,15 @@ pub fn confirm() -> Result<bool, Error> {
     let stdin = io::stdin();
 
     ctrlc::set_handler(move || {
-        println!("Bye...");
+        println!("{}", "Bye...".red());
         std::process::exit(1);
     })
     .expect("Error setting Ctrl-C handler");
 
-    println!("Press ENTER to confirm or Ctrl-C to cancel");
+    println!(
+        "{}",
+        "Press ENTER to confirm or Ctrl-C to cancel".blue().bold()
+    );
 
     match stdin.lock().read_line(&mut input) {
         Ok(_) => Ok(true),
